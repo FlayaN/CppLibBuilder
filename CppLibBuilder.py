c371148daf5e858c06c6981b8e3f19b531349044
FlayaN <hannes.feldt@gmail.com>
2014-11-06 21:22:40 +0100

    Load from mysql instead of local data.

--- CppLibBuilder.py
+++ CppLibBuilder.py
@@ -1,42 +1,66 @@
-import pymysql
+import mysql.connector
 from flask import Flask, jsonify, abort, make_response
 from flask.ext.httpauth import HTTPBasicAuth
+
+
+connection_config = {
+    'host': 'localhost',
+    'database': 'test',
+    'port': 3306,
+    'user': 'root',
+    'password': 'alpine',
+}
+
+class DataBaseManager(object):
+
+    def getLibraries(self):
+        conn = mysql.connector.connect(**connection_config)
+        cur = conn.cursor()
+
+        cur.execute("SELECT Name,Description, Version FROM Libs")
+        list = cur.fetchall()
+
+        cur.close()
+        conn.close()
+
+        return list
+    def getLibrary(self, lib_id):
+        conn = mysql.connector.connect(**connection_config)
+        cur = conn.cursor()
+
+        cur.execute("SELECT Name,Description, Version FROM Libs WHERE Id=%s", (lib_id,))
+        lib = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        return lib
+
 
 auth = HTTPBasicAuth()
 
 @auth.get_password
 def get_password(username):
-    if username == 'miguel':
-        return 'python'
+    if username == 'admin':
+        return 'pass'
     return None
 
+db = DataBaseManager()
 app = Flask(__name__)
 
-libs = [
-    {
-        'id': 1,
-        'title': u'Project 1',
-        'description': u'This is project 1',
-        'version': '1.0'
-    },
-    {
-        'id': 2,
-        'title': u'Project 2',
-        'description': u'This is project 2',
-        'version': '0.1'
-    }
-]
-
 @app.route('/api/libs', methods=['GET'])
-def get_libs():
-    return jsonify({'libs': libs})
+def get_libraries():
+    list = db.getLibraries()
+    if len(list) == 0:
+        abort(404)
+    return jsonify(libs = list)
 
 @app.route('/api/libs/<int:lib_id>', methods=['GET'])
-def get_lib(lib_id):
-    lib = list(filter(lambda t: t['id'] == lib_id, libs))
-    if len(lib) == 0:
+def get_library(lib_id):
+    lib = db.getLibrary(lib_id)
+    if lib is None:
         abort(404)
-    return jsonify({'lib': lib[0]})
+    return jsonify(lib=lib)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -44,23 +68,3 @@ def not_found(error):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='', db='mysql')
-#
-# cur = conn.cursor()
-#
-# cur.execute("SELECT Host,User FROM user")
-#
-# print(cur.description)
-#
-# for row in cur:
-#    print(row)
-#
-# cur.close()
-# conn.close()
-
-
-#def test():
-#    print("Hello world")
-
-#test()
