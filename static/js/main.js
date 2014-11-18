bdecd7ff1361c336795b80467956cf1ffd80a408
FlayaN <hannes.feldt@gmail.com>
2014-11-18 21:12:35 +0100

    Added basic knockout test.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,32 @@
-var test = "HEJ";
+function LibsViewModel() {
+    var self = this;
+    self.libs = ko.observableArray([
+        {
+            title: ko.observable('title #1'),
+            description: ko.observable('description #1'),
+            done: ko.observable(false)
+        },
+        {
+            title: ko.observable('title #2'),
+            description: ko.observable('description #2'),
+            done: ko.observable(true)
+        }
+    ]);
+
+    self.beginAdd = function() {
+        alert("Add");
+    }
+    self.beginEdit = function(lib) {
+        alert("Edit: " + lib.title());
+    }
+    self.remove = function(lib) {
+        alert("Remove: " + lib.title());
+    }
+    self.markInProgress = function(lib) {
+        lib.done(false);
+    }
+    self.markDone = function(lib) {
+        lib.done(true);
+    }
+}
+ko.applyBindings(new LibsViewModel());
